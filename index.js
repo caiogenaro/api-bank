6e7f2a17d8467a9b13e8d09846fc53045f681dfb
Caio <caiowgcw@gmail.com>
2021-02-06 00:48:14 -0300

    API BANK STARTED

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import accountsRouter from "./routes/accounts.js";
+import { promises as fs, write } from "fs";
+
+const { readFile, writeFile} = fs;
+
+const app = express();
+app.use(express.json());
+
+app.use("/account", accountsRouter);
+
+app.listen(3000, async () => {
+  try {
+    await readFile("accounts.json");
+    console.log("API Started");
+  } catch (err) {
+    console.log("err---------", err);
+    const initialJson = {
+      nextId: 1,
+      accounts: [],
+    };
+    writeFile("accounts.json", JSON.stringify(initialJson))
+      .then(() => {
+        console.log("API Started! and file created");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
